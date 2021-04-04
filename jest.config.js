86212f92e516db058a4825824e069b3df036a80d
denismart <denismart87@gmail.com>
2021-04-04 19:18:55 +0300

    v0.1.0

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    clearMocks: true,
-    testEnvironment: 'jsdom',
-    testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
-    testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-};
